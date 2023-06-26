1409a7e841a2bf006a41e1e1c35be83c955e8274
Niklas Korsbo <niklas.korsbo@gmail.com>
2023-06-26 17:38:44 +0000

    Tidy up initialization script

--- initialize.jl
+++ initialize.jl
@@ -1,19 +1,27 @@
 using DeepPumas
 
+@info "Compiling the machine learning exercises"
 @time redirect_stdout(devnull) do 
   include(dirname(@__FILE__()) * "/2_01_machine_learning.jl")
+  return nothing
 end
 
+@info "Compiling the neural IDR exercise"
 @time redirect_stdout(devnull) do 
   include(dirname(@__FILE__()) * "/2_02_idr.jl")
+  return nothing
 end
 
+@info "Compiling the UDE exercises"
 @time redirect_stdout(devnull) do 
   include(dirname(@__FILE__()) * "/2_03_SciML.jl")
+  return nothing
 end
 
+@info "Compiling the MeNet exercises"
 @time redirect_stdout(devnull) do 
   include(dirname(@__FILE__()) * "/2_04_MeNets.jl")
+  return nothing
 end
 
 @info "Finished compiling functions!"
